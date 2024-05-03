35e36f11bb4c947fd85a6569f259dcc02e3801a6
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-05-03 11:44:40 +0300

    [165] [Time Beats: 43.26%] [Memory Beats: 49.32%] - LeetPush

--- 165-Compare-Version-Numbers.py
+++ 165-Compare-Version-Numbers.py
@@ -1,20 +1,12 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        ver1 = list(map(lambda n: int(n), version1.split('.')))
-        ver2 = list(map(lambda n: int(n), version2.split('.')))
+        ver1 = list(map(int, version1.split('.')))
+        ver2 = list(map(int, version2.split('.')))
 
-        sz1, sz2 = len(ver1), len(ver2)
+        for a1, a2 in zip_longest(ver1, ver2, fillvalue=0):
+            if a1 == a2:
+                continue
+            return -1 if a2 > a1 else 1
 
-        if sz1 < sz2:
-            ver1.extend([0] * (sz2 - sz1))
-        elif sz1 > sz2:
-            ver2.extend([0] * (sz1 - sz2))
-
-        if ver1 > ver2:
-            return 1
-        elif ver2 > ver1:
-            return -1
-        
         return 0
-
         
