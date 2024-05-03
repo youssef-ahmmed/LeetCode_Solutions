d64b78f403a3ef02ffebff1c17787c225266fdd9
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-05-03 17:40:23 +0300

    [125] [Time Beats: 91.15%] [Memory Beats: 63.29%] - LeetPush

---
+++ 125-Valid-Palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+
+        i, j = 0, len(s) - 1
+
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+
+            if s[i] != s[j]:
+                return False
+
+            i += 1
+            j -= 1
+
+        return True
