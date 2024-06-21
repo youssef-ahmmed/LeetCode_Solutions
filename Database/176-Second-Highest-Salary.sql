3120f7e664e41beb1e84dcaa12c4c8a0a9f481a0
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-06-22 00:49:49 +0300

    [176] [Time Beats: undefined] - LeetPush

---
+++ Database/176-Second-Highest-Salary.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    MAX(salary) AS SecondHighestSalary
+FROM
+    Employee
+WHERE 
+    salary < (
+        SELECT
+            MAX(salary)
+        FROM   
+            Employee
+    );
