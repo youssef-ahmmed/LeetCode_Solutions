b4a4329ade4834b9ce42912059029914206e2590
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-07-03 16:32:42 +0300

    [2626] [Time Beats: 13.14%] [Memory Beats: 85.25%] - LeetPush

---
+++ JavaScript Problems/2626-Array-Reduce-Transformation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let acc = init;
+    for (let i = 0; i < nums.length; i++) {
+        acc = fn(acc, nums[i]);
+    }
+
+    return acc;
+};
