6a1f040e33df119cd17247efcd05c4635296815f
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-07-03 16:31:09 +0300

    [2634] [Time Beats: 92.08%] [Memory Beats: 73.03%] - LeetPush

---
+++ JavaScript Problems/2634-Filter-Elements-from-Array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let output = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            output.push(arr[i]);
+        }
+    }
+
+    return output;
+};
