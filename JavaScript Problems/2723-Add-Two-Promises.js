592a8279f952340761c44c584ea51a9382d1f149
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-07-13 01:53:34 +0300

    [2723] [Time Beats: 79.31%] [Memory Beats: 31.27%] - LeetPush

---
+++ JavaScript Problems/2723-Add-Two-Promises.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const value1 = await promise1;
+    const value2 = await promise2;
+
+    return new Promise(resolve => resolve(value1 + value2));
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
