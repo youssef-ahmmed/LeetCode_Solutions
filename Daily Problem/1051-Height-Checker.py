a75059c4a81e70d185b95c29db0ed633d8ada846
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-07-24 23:12:07 +0300

    [1051] [Time Beats: 50.75%] [Memory Beats: 13.08%] - LeetPush

---
+++ Daily Problem/1051-Height-Checker.py
@@ -0,0 +1,10 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+
+        cnt = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                cnt += 1
+
+        return cnt
