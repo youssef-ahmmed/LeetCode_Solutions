465e5ee77bec32e0d7d0415f4a35a9f3f47b02e5
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-08-15 09:06:36 +0300

    [860] [Time Beats: 78.10%] [Memory Beats: 58.06%] - LeetPush

---
+++ Daily Problem/860-Lemonade-Change.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = ten = 0
+
+        for b in bills:
+            if b == 5:
+                five += 1
+            elif b == 10:
+                five, ten = five - 1, ten + 1
+            elif ten > 0:
+                five, ten = five - 1, ten - 1
+            else:
+                five -= 3
+
+            if five < 0:
+                return False
+
+        return True
