0357e54cdc544462844b34d6e9a05cafa290684e
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-09-09 23:30:48 +0300

    [973] [Time Beats: 67.74%] [Memory Beats: 88.59%] - LeetPush

--- Problems/973-K-Closest-Points-to-Origin.py
+++ Problems/973-K-Closest-Points-to-Origin.py
@@ -2,7 +2,7 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
         def calc_distance(point):
-            return math.sqrt(point[0] ** 2 + point[1] ** 2)
+            return point[0] ** 2 + point[1] ** 2
 
         distance = []
 
