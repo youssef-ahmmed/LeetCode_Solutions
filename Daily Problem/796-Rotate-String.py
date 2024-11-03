6d557d161584c6c072cc8becbd62c22c36a9e318
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-11-03 06:10:57 +0200

    [796] [Time Beats: 100.00%] [Memory Beats: 40.35%] - LeetPush

--- Daily Problem/796-Rotate-String.py
+++ Daily Problem/796-Rotate-String.py
@@ -1,14 +1,3 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        n = len(s)
-
-        if len(s) != len(goal):
-            return False
-
-        for i in range(n):
-            if goal[i] == s[0]:
-                if ((s[:i] == goal[n - i:] and s[i:] == goal[:n - i])
-                    or (goal[:i] == s[n - i:] and goal[i:] == s[:n - i])):
-                    return True
-        
-        return False
+        return len(s) == len(goal) and goal in s * 2
