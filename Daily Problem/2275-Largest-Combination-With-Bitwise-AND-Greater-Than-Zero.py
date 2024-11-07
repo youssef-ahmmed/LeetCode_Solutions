f72f2e44cc9d32fc2d1b536220ef08bdbdd9de05
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-11-07 22:06:40 +0200

    [2275] [Time Beats: 69.41%] [Memory Beats: 81.46%] - LeetPush

---
+++ Daily Problem/2275-Largest-Combination-With-Bitwise-AND-Greater-Than-Zero.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        res = 0
+
+        for i in range(24):
+            cnt = 0
+            for n in candidates:
+                cnt += 1 if (1 << i) & n else 0
+
+            res = max(res, cnt)
+
+        return res
