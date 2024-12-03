cf0cfbbc0297f81f74a501b7d7fadbcc307f4ebf
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-12-03 20:17:11 +0200

    [2109] [Time Beats: 47.23%] [Memory Beats: 36.87%] - LeetPush

--- Problems/2109-Adding-Spaces-to-a-String.py
+++ Problems/2109-Adding-Spaces-to-a-String.py
@@ -1,12 +1,12 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-        new_str = ''
+        res = []
 
         j = 0
         for i, c in enumerate(s):
             if j < len(spaces) and i == spaces[j]:
-                new_str += ' '
+                res.append(' ')
                 j += 1
-            new_str += c
+            res.append(c)
 
-        return new_str
+        return ''.join(res)
