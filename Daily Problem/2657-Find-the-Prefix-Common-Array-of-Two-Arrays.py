a82f5d432b0ee796c55798b9c8cd5360996aa105
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2025-01-14 23:41:24 +0200

    [2657] [Time Beats: 94.24%] [Memory Beats: 32.72%] - LeetPush

--- Daily Problem/2657-Find-the-Prefix-Common-Array-of-Two-Arrays.py
+++ Daily Problem/2657-Find-the-Prefix-Common-Array-of-Two-Arrays.py
@@ -1,16 +1,20 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        res = []
+        n = len(A)
+        res = [0] * n
         set_a = set()
         set_b = set()
-        common = set()
-
-        for i in range(len(A)):
-            set_a.add(A[i])
-            set_b.add(B[i])
-
-            common.update(set_a & set_b)
-
-            res.append(len(common))
-
+        
+        for i in range(n):
+            res[i] = res[i - 1]
+            if A[i] == B[i]:
+                res[i] += 1
+            else:
+                if A[i] in set_b:
+                    res[i] += 1
+                if B[i] in set_a:
+                    res[i] += 1
+                set_a.add(A[i])
+                set_b.add(B[i])
+            
         return res
