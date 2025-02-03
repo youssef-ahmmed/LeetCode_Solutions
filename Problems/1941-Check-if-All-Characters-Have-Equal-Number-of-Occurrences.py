2fec85741c6be90552d12b04df5b2ade8428732e
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2025-02-03 13:19:37 +0200

    [1941] [Time Beats: 100.00%] [Memory Beats: 68.02%] - LeetPush

--- Problems/1941-Check-if-All-Characters-Have-Equal-Number-of-Occurrences.py
+++ Problems/1941-Check-if-All-Characters-Have-Equal-Number-of-Occurrences.py
@@ -1,9 +1,4 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
         count = Counter(s)
-        values = list(count.values())
-
-        for i in range(len(values) - 1):
-            if values[i] != values[i + 1]:
-                return False
-        return True
+        return len(set(count.values())) == 1
