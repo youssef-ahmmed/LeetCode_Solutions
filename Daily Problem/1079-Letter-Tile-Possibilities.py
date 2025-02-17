bb631fb661bc585fa165e742887ab944642a4e58
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2025-02-17 22:29:45 +0200

    [1079] [Time Beats: 44.40%] [Memory Beats: 83.13%] - LeetPush

---
+++ Daily Problem/1079-Letter-Tile-Possibilities.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        count = Counter(tiles)
+
+        def backtrack():
+            res = 0
+
+            for c in count:
+                if count[c] > 0:
+                    count[c] -= 1
+                    res += 1
+                    res += backtrack()
+                    count[c] += 1
+            
+            return res
+        
+        return backtrack()
