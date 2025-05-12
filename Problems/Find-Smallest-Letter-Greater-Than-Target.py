1ef38ba604c15e1400555a050555282bf658aefe
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2025-05-12 21:13:29 +0300

    [744] [Time Beats: 100.00%] [Memory Beats: 6.01%] - LeetPush

---
+++ Problems/Find-Smallest-Letter-Greater-Than-Target.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        l, r = 0, len(letters)
+
+        while l < r:
+            m = l + (r - l) // 2
+
+            if letters[m] <= target:
+                l = m + 1
+            else:
+                r = m
+
+        return letters[l] if l < len(letters) else letters[0]
